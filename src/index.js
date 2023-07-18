ebbc200ec14979c6a331856666aa1e984fb82833
Jose Cepero <ceperojose54@gmail.com>
2023-07-17 20:44:59 -0400

    wrapped app in provider and made store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducer from './reducers';
+
+const store = createStore(reducer);
 
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
+  <Provider store={store}>
+    <Router>
+
       <App />
-  </Router>,
+
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
